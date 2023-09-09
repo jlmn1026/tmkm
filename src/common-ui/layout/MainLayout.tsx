084fb56b47347946dee587efd5fca04ce0d8f08e
HalJune1026 <jh.dev.acc.1026.i2k9@gmail.com>
2023-09-09 20:47:29 +0900

    feat: add Menu open close anime

--- src/common-ui/layout/MainLayout.tsx
+++ src/common-ui/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
-import { styled } from '@/stitches.config.ts';
-import { MenuOutlined } from '@ant-design/icons';
+import { ReactNode, useState } from 'react';
+import { keyframes, styled } from '@/stitches.config.ts';
+import { CloseOutlined, MenuOutlined } from '@ant-design/icons';
 import HeadlessButton from '../HeadlessButton';
 
 type Props = {
@@ -8,19 +8,38 @@ type Props = {
 };
 
 const MainLayout = ({ children }: Props) => {
+  const [openMenu, setOpenMenu] = useState<'start' | 'show' | 'hide'>('start');
   return (
     <>
+      {openMenu === 'show' && (
+        <MenuBackground
+          onClick={() => {
+            setOpenMenu('hide');
+          }}
+        />
+      )}
       <Header>
         <HeaderContent>
           <HeadlessButton
             onClick={() => {
-              alert(1);
+              setOpenMenu('show');
             }}
           >
             <MenuOutlined style={{ color: '#fff' }} />
           </HeadlessButton>
         </HeaderContent>
       </Header>
+      <MenuSlider open={openMenu}>
+        <MenuHeader>
+          <HeadlessButton
+            onClick={() => {
+              setOpenMenu('hide');
+            }}
+          >
+            <CloseOutlined style={{ color: '#fff' }} />
+          </HeadlessButton>
+        </MenuHeader>
+      </MenuSlider>
       {children}
     </>
   );
@@ -38,4 +57,52 @@ const HeaderContent = styled('div', {
   height: '100%',
   alignItems: 'center',
   marginLeft: '12px',
+});
+
+const showMenu = keyframes({
+  from: { left: '-250px' },
+  to: { left: '0px', visibility: 'visible' },
+});
+
+const hideMenu = keyframes({
+  from: { left: '0px' },
+  to: { left: '-250px', visibility: 'hidden' },
+});
+
+const MenuSlider = styled('div', {
+  variants: {
+    open: {
+      show: {
+        animation: `${showMenu} 250ms`,
+      },
+      hide: {
+        animation: `${hideMenu} 250ms`,
+        left: '-250px',
+      },
+      start: {
+        left: '-250px',
+      },
+    },
+  },
+
+  position: 'absolute',
+  top: '0px',
+  background: '#223344',
+  height: '100vh',
+  width: '250px',
+});
+
+const MenuHeader = styled('div', {
+  display: 'flex',
+  justifyContent: 'end',
+  padding: '12px',
+});
+
+const MenuBackground = styled('div', {
+  position: 'absolute',
+  top: '0px',
+  left: '0px',
+  width: '100vw',
+  height: '100vh',
+  opacity: 0.7,
 });
