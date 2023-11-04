e51a6ba5b454aef051572b71ec1b6f2b096d7ebb
HalJune1026 <jh.dev.acc.1026.i2k9@gmail.com>
2023-11-04 17:16:17 +0900

    feat: prepare build dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/": `${__dirname}/src/`, 
+      '@/': `${__dirname}/src/`,
     },
-  }
-})
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+  },
+});
